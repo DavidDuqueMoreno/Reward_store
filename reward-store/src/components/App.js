032ffec0ca7a75bed495dfc4ab4818950fc8a984
debaranpaisa <debaranhemoso@gmail.com>
2021-01-13 23:01:06 -0500

    Componente Header y carpeta assets
    Se creo el componente header dentro de la carpeta components usando jsx y css y se importo en App.js y se añadio la carpeta assets dentro de  la carpeta src, la cual contiene las imagenes y logos que se van a utilizar en el proyecto.

---
+++ reward-store/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../index.css';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+// import Main from './Main';
+
+const App = () => (
+	<BrowserRouter>
+		<Header />
+		<div>
+			{/* <Route exact path="/1" component={Main} /> */}
+			{/* <Route exact path="/2" component={Main} /> */}
+		</div>
+	</BrowserRouter>
+);
+
+export default App;
---
+++ reward-store/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Kite from './assets/aerolab-logo.svg';
+import Coin from './assets/icons/coin.svg';
+import Hero from './assets/header-x1.png';
+
+const Header = () => {
+	return (
+		<div>
+			<div className="header_info_container">
+				<img src={Kite} alt="aerolab-logo" />
+				<div className="header_user_container">
+					<div className="header_user_name">David Duque</div>
+					<div className="header_user_coins">
+						6000
+						<div className="coin_icon">
+							<img src={Coin} alt="coin icon" />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="header_hero_container">
+				<img className="header_hero_img" src={Hero} alt="hero img" />
+				<div className="header_hero_text">Electronics</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
